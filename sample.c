90e2352c17279db764925124a260b953a898116b
linux1 <linux123@gmail.com>
2022-05-23 10:30:54 +0530

    first commit of sample.c

---
+++ sample.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+int main(int argc,char* argv[])
+{ 
+  printf("current process id is : %d\n",getpid()); 
+  system("ps -ef");
+  printf("No of arguments passed : %d\n",argc);
+  for(int i=0;i<argc;i++)
+     printf("%s \n",argv[i]);
+
+}	
